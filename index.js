8143fa00d560b6e644cd3919419fc38484572964
Rebornian48 <80138640+Rebornian48@users.noreply.github.com>
2024-02-12 16:20:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ app.get("/versi", (req, res) => {
     res.send("Versi 1.0.0");
 });
 
+app.get("/author", (req, res) => {
+  res.send("Rebornian48");
+});
+
+app.get("/kategori", (req, res) => {
+  const kat = [{
+    id:1,
+    nama:'member'  
+  },{
+    id:2,
+    nama:'lagu'
+  },{
+    id:3,
+    nama:'lokasi_teater'
+  },{
+    id:2,
+    nama:'lokasi_konser'
+  }
+]
+  res.send(kat);
+});
+
 app.listen(port, () => {
   console.log(`Aplikasi memakai port ${port}`);
 });
